a2e0eb94cb0dee7332209611e433c0394dc5714e
Redwan Rashid Rico <22303307@iubat.edu>
2025-04-07 12:14:48 +0600

    Fixed more project files for cpanel

--- public/.env.php
+++ public/.env.php
@@ -8,7 +8,7 @@
 
 // Supabase configuration
 $_ENV['SUPABASE_URL'] = 'https://mgqtlgpcdswfmvgheeff.supabase.co';
-$_ENV['SUPABASE_ANON_KEY'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ncXRsZ3BjZHN3Zm12Z2hlZWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzY3ODAzNjIsImV4cCI6MTk5MjM1NjM2Mn0.8Rq8VbucEWbJUZg2XfxNBxQyAYZl5eEYWCifoZ2lQ_o';
+$_ENV['SUPABASE_ANON_KEY'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ncXRsZ3BjZHN3Zm12Z2hlZWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM5NjUxOTUsImV4cCI6MjA1OTU0MTE5NX0.bIQBXBmi4x1GYjh7nFRZ0vcFb-Rs2yn2hKMROxt5r3w';
 
 // Application settings
 $_ENV['APP_VERSION'] = '1.0.0';
